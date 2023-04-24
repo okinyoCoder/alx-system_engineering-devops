2d216e8c2f8eb8a75b1ff6fb8039b04e5a1dfba9
okinyoCoder <okinyoanthony@gmail.com>
2023-04-24 17:22:18 +0000

    correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Creates a file
+# puppet script that creates a file in /tmp
 
 file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
+  content => 'I love Puppet',
+  mode    =>  '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
