8ae0f2c96b20cf4fefb22115bf113316143f49b5
okinyoCoder <okinyoanthony@gmail.com>
2023-05-23 07:24:26 +0000

    debugged

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,12 +4,14 @@
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
     if len(argv) > 1:
         url = 'https://jsonplaceholder.typicode.com'
-        number = argv[1]
+        nu = argv[1]
         '''getting username from api'''
-        r = requests.get('{}/users/{}'.format(url, number))
+        r = requests.get('{}/users/{}'.format(url, nu))
+
         name = r.json().get('name')
         '''getting total number of task'''
         req = requests.get('{}/todos?userId={}'.format(url, number))
@@ -22,7 +24,7 @@ if __name__ == "__main__":
                 count = count + 1
                 lizt.append(obj.get('title'))
 
-        print('Employee {} is done with tasks ({}/{}):'.format(
+        print('Employee {} is done with tasks ({}/{})'.format(
             name, count, total_task))
 
         for i in lizt:
