8792e45762ef489e00a1786276527b01ef76424a
okinyoCoder <okinyoanthony@gmail.com>
2023-05-23 05:15:00 +0000

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that gathers data from API"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        url = 'https://jsonplaceholder.typicode.com'
+        number = argv[1]
+        '''getting username from api'''
+        r = requests.get('{}/users/{}'.format(url, number))
+        name = r.json().get('name')
+        '''getting total number of task'''
+        req = requests.get('{}/todos?userId={}'.format(url, number))
+        task = req.json()
+        total_task = len(task)
+        count = 0
+        lizt = []
+        for obj in task:
+            if obj.get('completed') == True:
+                count = count + 1
+            lizt.append(obj.get('title'))
+        print('Employee {} is done with tasks ({}/{})'
+                .format(name, count, total_task))
+        for i in lizt:
+            print(i)
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+ API
+Python, Scripting and Back-end
