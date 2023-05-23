648c55869249fc1f0857b4cd03dc7bc674a60da3
okinyoCoder <okinyoanthony@gmail.com>
2023-05-23 07:29:13 +0000

    debugged

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
         nu = argv[1]
         '''getting username from api'''
         r = requests.get('{}/users/{}'.format(url, nu))
-
         name = r.json().get('name')
         '''getting total number of task'''
-        req = requests.get('{}/todos?userId={}'.format(url, number))
+        req = requests.get('{}/todos?userId={}'.format(url, nu))
         task = req.json()
         total_task = len(task)
         count = 0
@@ -23,7 +22,6 @@ if __name__ == "__main__":
             if obj.get('completed') is True:
                 count = count + 1
                 lizt.append(obj.get('title'))
-
         print('Employee {} is done with tasks ({}/{})'.format(
             name, count, total_task))
 
