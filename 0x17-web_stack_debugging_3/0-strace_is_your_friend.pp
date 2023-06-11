3310d55638c3522c0f71e6de90ae0988fdb764d3
okinyoCoder <okinyoanthony@gmail.com>
2023-06-11 23:53:53 +0000

    php config

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# use this script to fix typo in config
+
+exec { 'fix config':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
